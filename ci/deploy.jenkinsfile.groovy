8bbe93500d5f7794c428e31df6659a34b7f46bdc
samavasi <samavasi@hotmail.com>
2022-03-22 16:31:26 +0700

    some placeholder pipeline files

---
+++ ci/deploy.jenkinsfile.groovy
@@ -0,0 +1,4 @@
+#!/usr/bin/env groovy
+
+//TODO add parameter to choose from available versions
+//Maybe since monorepo same version strategy for all, so each commit becomes a version to deploy?
---
+++ ci/release.jenkinsfile.groovy
@@ -0,0 +1,4 @@
+#!/usr/bin/env groovy
+
+//TODO we can add a pipeline to cut a release (tag+commit to the repo) if versioning strategy is not to release/deploy on any new commit
+//(i.e. only collections of commits make up a meaningful moment to release)
