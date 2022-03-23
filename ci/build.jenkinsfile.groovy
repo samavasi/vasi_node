dbae42b18a41545ba4fee718602cd606682acf70
samavasi <samavasi@hotmail.com>
2022-03-23 16:05:18 +0700

    basic build pipeline

--- ci/build.jenkinsfile.groovy
+++ ci/build.jenkinsfile.groovy
@@ -1,3 +1,33 @@
-#!/usr/bin/env groovy
-
-//TODO 
+pipeline {
+    agent none
+    options {
+        // Add a global timeout to make sure we dont get builds hanging forever
+        timeout(time: 2, unit: 'HOURS')
+    }
+    stages {
+        stage('Build/Publish application image') {
+            agent any
+            steps {
+                sh 'sudo docker build -t localhost:5001/xendit-demo-nodejs . && sudo docker push localhost:5001/xendit-demo-nodejs'
+            }
+        }
+        //TODO parallel lint/test helm stuff, docker stuff
+        stage('Tests/validations') {
+            failFast false
+            parallel {
+                stage('Branch A') {
+                    agent any
+                    steps {
+                        echo "On Branch A"
+                    }
+                }
+                stage('Branch B') {
+                    agent any
+                    steps {
+                        echo "On Branch B"
+                    }
+                }
+            }
+        }
+    }
+}
