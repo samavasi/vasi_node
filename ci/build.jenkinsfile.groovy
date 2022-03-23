6b1f978d1b25e733a4b2d3b407d4557033d7d9e7
samavasi <samavasi@hotmail.com>
2022-03-24 00:15:26 +0700

    updating build pipeline

--- ci/build.jenkinsfile.groovy
+++ ci/build.jenkinsfile.groovy
@@ -5,29 +5,70 @@ pipeline {
         timeout(time: 2, unit: 'HOURS')
     }
     stages {
-        stage('Build/Publish application image') {
+        stage('Build application image') {
             agent any
+            //TODO get/set version (commitSha + semver?)
             steps {
-                sh 'sudo docker build -t localhost:5001/xendit-demo-nodejs . && sudo docker push localhost:5001/xendit-demo-nodejs'
+                sh 'sudo docker build -t localhost:5001/xendit-demo-nodejs .'
             }
         }
-        //TODO parallel lint/test helm stuff, docker stuff
         stage('Tests/validations') {
             failFast false
             parallel {
-                stage('Branch A') {
+                stage('Helm Lint application chart') {
                     agent any
                     steps {
-                        echo "On Branch A"
+                        sh "helm lint --strict ./xendit-demo-nodejs"
                     }
                 }
-                stage('Branch B') {
+                stage('Run Kubeval for application chart manifests') {
                     agent any
                     steps {
-                        echo "On Branch B"
+                        sh "helm kubeval ./xendit-demo-nodejs"
+                    }
+                }
+                stage('Run Checkov scan for application chart manifests') {
+                    agent any
+                    steps {
+                        sh "checkov --directory ./xendit-demo-nodejs —framework helm"
+                    }
+                }
+                stage('Run Checkov scan against application Dockerfile') {
+                    agent any
+                    steps {
+                        sh "checkov --file Dockerfile"
                     }
                 }
             }
         }
+        stage('Publish application image') {
+            agent any
+            steps {
+                // NOTE: we only have 1 node so no need to try to build again here for simplicity just pushing
+                sh 'sudo docker push localhost:5001/xendit-demo-nodejs'
+            }
+        }
+        stage('Smoke deploy/test helm chart') {
+            agent any
+            when { changeRequest() }
+            steps {
+                sh "helm upgrade --namespace myapp --create-namespace --install smokedeploy-xendit-demo-${env.PR} --wait ./xendit-demo-nodejs -f ./xendit-demo-nodejs/values.yaml"
+                sh "helm test smokedeploy-xendit-demo-${env.PR}"
+            }
+            post {
+                always {
+                    sh "helm uninstall smokedeploy-xendit-demo-${env.PR}"
+                }
+            }
+        }
+        // stage('Deploy to dev') {
+        //     input {
+        //         message "Deploy to dev?"
+        //         ok "Deploy"
+        //     }
+        //     steps {
+        //         echo "Hello, ${PERSON}, nice to meet you."
+        //     }
+        // }
     }
 }
