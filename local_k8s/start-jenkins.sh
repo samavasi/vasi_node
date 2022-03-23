50557133064dbe0ada59278b943bb0b4acf249f4
samavasi <samavasi@hotmail.com>
2022-03-23 16:41:17 +0700

    adding helm/kubectl in jenkins image

--- local_k8s/start-jenkins.sh
+++ local_k8s/start-jenkins.sh
@@ -7,4 +7,5 @@ docker run -p 8080:8080 \
     -p 50000:50000 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v jenkins_home:/var/jenkins_home \
+    -v /Users/samavasi/.kube/config:/var/jenkins_home/.kube/config \
     -d jenkins-docker
