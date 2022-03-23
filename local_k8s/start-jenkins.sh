49e753ff7b6bbf014f19a52bf0d81af06a484067
samavasi <samavasi@hotmail.com>
2022-03-23 15:40:02 +0700

    add custom Dockerfile and script used to run my local jenkins

---
+++ local_k8s/start-jenkins.sh
@@ -0,0 +1,10 @@
+# start custom jenkins image (plugins and some settings are already in stored in mounted docker volume)
+# https://www.jenkins.io/doc/book/installing/docker/
+# https://blog.container-solutions.com/running-docker-in-jenkins-in-docker
+docker build -t jenkins-docker .
+
+docker run -p 8080:8080 \
+    -p 50000:50000 \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v jenkins_home:/var/jenkins_home \
+    -d jenkins-docker
